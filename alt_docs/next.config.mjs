4cddcada26de8efd485d6dee0c7408c57a3d0083
drdavidg <david@curiousminds.com>
2025-06-09 12:34:40 -0700

    Docs files and folders reorganized

--- alt_docs/next.config.mjs
+++ alt_docs/next.config.mjs
@@ -13,7 +13,7 @@ const withMDX = nextMDX({
   },
 })
 
-/** @type {import('next').NextConfig} */
+/** @type {import('./protocol_docs/node_modules/next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   experimental: {
