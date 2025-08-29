997d932539b310d92657696fca1540be6d6b0080
kasim <kakasimenator@gmail.com>
2025-08-29 08:55:50 +0300

    session status and settings webhooks

--- src/types/webhooks.ts
+++ src/types/webhooks.ts
@@ -1,11 +1,12 @@
 export enum WebhookType {
-  SESSION_STARTED = "session.started",
-  SESSION_FINISHED = "session.finished",
+  SESSION_STATUS = "session.status",
   MEDIA_USER_PHOTO = "media.user_photo",
   MEDIA_DOCUMENT_PHOTO = "media.document_photo",
   MEDIA_USER_VIDEO = "media.user_video",
   ANALYSIS_VIDEO = "analysis.video",
   ANALYSIS_SCREENSHOT = "analysis.screenshot",
+  SETTINGS_AVATARS = "settings.avatars",
+  SETTINGS_LANGS = "settings.langs",
 }
 
 export type WebhookMedia = {
